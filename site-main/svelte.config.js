f86f363d3e8b4d148c093f4da4450ff8c7aa4d27
sockheadrps <r.p.skiles@gmail.com>
2025-03-18 12:57:30 -0400

    node adapter

--- site-main/svelte.config.js
+++ site-main/svelte.config.js
@@ -1,5 +1,8 @@
 // import adapter from '@sveltejs/adapter-auto'
-import adapter from '@sveltejs/adapter-static'
+// import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-node'
+
+
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { fileURLToPath } from 'url';
