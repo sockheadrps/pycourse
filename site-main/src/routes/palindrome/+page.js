15efabc9d7ed2279b972e34fe52a42c8459dfd6b
sock <r.p.skiles@gmail.com>
2025-04-17 02:41:35 -0400

    Palindrome RNN artical

---
+++ site-main/src/routes/palindrome/+page.js
@@ -0,0 +1,16 @@
+import { error } from '@sveltejs/kit';
+
+export async function load({ params }) {
+	try {
+		const about = await import(`../../static/palindrome/palindrome.svx`);
+		console.log(about);
+
+		return {
+			content: about.default,
+			meta: about.metadata
+		};
+	} catch (e) {
+		console.error(e);
+		throw error(404, 'Post not found');
+	}
+}
---
+++ site-main/src/routes/palindrome/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="js">
+	import { formatDate } from '$lib/utils';
+
+	export let data;
+</script>
+
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={data.meta.title} />
+</svelte:head>
+
+<article>
+	<hgroup>
+		<h1>{data.meta.title}</h1>
+		<p>Published on {formatDate(data.meta.date)}</p>
+	</hgroup>
+
+	<div class="tags">
+		{#each data.meta.categories as category}
+			<span class="surface-4">&num;{category}</span>
+		{/each}
+	</div>
+
+	<div class="prose">
+		<svelte:component this={data.content} />
+	</div>
+</article>
+
+<style>
+	article {
+		margin-inline: auto;
+		color: var(--text-1);
+		width: 1200px;
+	}
+
+	h1 {
+		text-transform: capitalize;
+		color: var(--text-1);
+	}
+
+	h1 + p {
+		margin-top: var(--size-2);
+		color: var(--text-2);
+	}
+
+	.tags {
+		display: flex;
+		gap: var(--size-3);
+		margin-top: var(--size-7);
+	}
+
+	.tags > * {
+		padding: var(--size-2) var(--size-3);
+		border-radius: var(--radius-round);
+		background-color: var(--surface-2);
+		color: var(--text-1);
+	}
+
+	.about {
+		font-size: larger;
+		padding: 14px;
+		color: var(--text-2);
+	}
+
+	.prose {
+		color: var(--text-2);
+	}
+</style>
